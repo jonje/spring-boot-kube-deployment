60ee3a2cc8dc2f07adada8f4a6c1196fc4f779ee
Jonathan Jensen <haiyun211@gmail.com>
2022-09-29 20:18:16 -0600

    Initial Commit

---
+++ src/main/java/org/haknet/example/springbootkubedeployment/SpringBootKubeDeploymentApplication.java
@@ -0,0 +1,13 @@
+package org.haknet.example.springbootkubedeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKubeDeploymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKubeDeploymentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/haknet/example/springbootkubedeployment/SpringBootKubeDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.haknet.example.springbootkubedeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKubeDeploymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
